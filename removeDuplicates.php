d19ffbbdb0a05792c55fb2bbad004384f2ce8ff0
sarkarmamun171 <sarkar.pau.cse171@gmail.com>
2024-06-30 23:54:03 +0600

    Remove Duplicates in PHP

---
+++ removeDuplicates.php
@@ -0,0 +1,25 @@
+<?php
+function removeDuplicates(&$nums) {
+    $n = count($nums);
+    if ($n == 0) return 0;
+
+    $j = 0;
+    for ($i = 1; $i < $n; $i++) {
+        if ($nums[$i] != $nums[$j]) {
+            $j++;
+            $nums[$j] = $nums[$i];
+        }
+    }
+
+    // Fill remaining elements with underscores (optional, for clarity)
+    for ($i = $j + 1; $i < $n; $i++) {
+        $nums[$i] = '_';
+    }
+
+    return $j + 1;
+}
+
+$nums = [1, 1, 2];
+$length = removeDuplicates($nums);
+echo "Output: $length, nums = [" . implode(", ", $nums) . "]\n";
+?>
